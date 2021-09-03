a991e648b69634cbeb884559c8b9e7f617faca83
4lequinn <jorgealequinn@gmail.com>
2021-09-03 13:02:44 -0400

    Version 5

--- src/app/pages/pagina2/pagina2.page.ts
+++ src/app/pages/pagina2/pagina2.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+//
+import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-pagina2',
   templateUrl: './pagina2.page.html',
@@ -7,16 +10,45 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class Pagina2Page implements OnInit {
 
-  constructor() { }
+  constructor(private activatedRoute :  ActivatedRoute) { }
 
   ngOnInit() {
+    // Recibimos el parametro
+    var valor = this.activatedRoute.snapshot.paramMap.get('valor');
+    this.buscar(valor);
+    alert('El valor es ' + valor)
   }
 
   persona = new FormGroup(
     {
-      elRut: new FormControl(''),
-      elNombreCompleto: new FormControl(''),
-      laEdad: new FormControl('')
+      formRut: new FormControl(''),
+      formNombreCompleto: new FormControl(''),
+      formEdad: new FormControl('')
     }
   )
+  
+  perso:any = {
+    rut:'293892',
+  };
+
+  buscar(rut:String){
+    var datos = localStorage.getItem('misdatos');
+
+    // LISTAR
+    datos = datos.replace('[','');
+    datos = datos.replace(']','');
+    datos = datos.split('},{').join('};{');
+    var arreglo_temp = datos.split(";");
+    for (let index = 0; index < arreglo_temp.length; index++) {
+      var registro = arreglo_temp[index];
+      var la_persona = JSON.parse(registro);
+      if(la_persona.rut == rut){
+        this.perso = {
+          rut: la_persona.rut,
+        };
+        this.persona.controls.formNombreCompleto.setValue(la_persona.nombre)
+        this.persona.controls.formEdad.setValue(la_persona.edad)
+      }
+    }
+  }
 }
